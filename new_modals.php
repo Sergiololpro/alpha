6e10c1e727f49869dcc83b57d364e026e3937798
Sergiololpro <homerjsimp@yandex.ru>
2020-07-02 21:28:53 +0300

    More modals

--- new_modals.php
+++ new_modals.php
@@ -10,9 +10,7 @@
         <img src="#" data-src="img/new_modal__helm-mask.png" alt="" class="new_modal__helm new_modal__helm-mask lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">RESPO MASK</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">Our new approach to a ventilation and helmet microclimate control brings purer air quality for the rider’s comfort. The ventilated mask draws in fresh air through its dedicated front port featuring a filtration layer to eliminate pollen, soot, dust, and other pollutants whilst exhaust carbon dioxide is released through side vents, preventing excessive visor fog in wet or hot conditions. The Respo Mask clips on to a front chin bar.</div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -24,9 +22,7 @@
         <img src="#" data-src="img/new_modal__helm-light-small.png" alt="" class="new_modal__helm new_modal__helm-light new_modal__helm-light-small lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">Break light</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">Wireless brake detection technology, designed to be seen in poor visibility weather such as fog and hail, whilst maintaining aerodynamics. Automatic sensors warn other road users when the rider is braking or down-shifting gears. The emergency beacon flashes in-case of an impact and can be reset manually.</div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -38,9 +34,7 @@
         <img src="#" data-src="img/new_modal__helm-air-small.png" alt="" class="new_modal__helm new_modal__helm-air new_modal__helm-air-small lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">AIR INTAKES</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">The Alpha Pro’s strategically placed air vents ensure a cooler, more ventilated riding experience. The helmet’s aerodynamically tuned shell delivers optimum stability and reduces fatigue. The front vents are designed to capture maximum airflow in the front of the shell and increase air pressure on the rear and side exhaust ports, thereby increasing the velocity of air transmission inside the helmet.</div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -51,9 +45,7 @@
         <img src="#" data-src="img/new_modal__helm-carbon-mob.png" alt="" class="new_modal__helm new_modal__helm-carbon new_modal__helm-carbon-mob lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">CARBON SHELL</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">The helmet features an extremely compact and light shell, manufactured from proven 3K aviation grade carbon, providing extra strength. Riders experience the performance and feel of a true race helmet.</div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -64,9 +56,9 @@
         <img src="#" data-src="img/new_modal__helm-spoiler-mob.png" alt="" class="new_modal__helm new_modal__helm-spoiler new_modal__helm-spoiler-mob lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">Spoiler</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">This ground-breaking innovation in helmet aerodynamics allows the user to manually switch the spoiler to 1 of the 3 positions, maximising aerodynamic performance and adding stability at high speed whilst reducing noise and buffeting. (Patent Pending)<br />
+            Please Note! Helmet comes with a standard fixed non-adjustable spoiler.  Adjustable Aero Wing option can be Added during the ordering process.
+            </div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -77,9 +69,9 @@
         <img src="#" data-src="img/new_modal__helm-glasses-small.png" alt="" class="new_modal__helm new_modal__helm-glasses new_modal__helm-glasses-small lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">GLASSES</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">Wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.<br />
+            Emergency beacon wile flash in-case of an accident
+            which can be reset manually.</div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -89,9 +81,9 @@
         <img src="#" data-src="img/new_modal__helm-blue.png" alt="" class="new_modal__helm new_modal__helm-blue lazyload">
         <div class="new_modal__content">
             <div class="new_modal__title">Bluetooth</div>
-            <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            <div class="new_modal__text">Wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.<br />
+            Emergency beacon wile flash in-case of an accident
+            which can be reset manually.</div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -104,8 +96,93 @@
         <div class="new_modal__content">
             <div class="new_modal__title">Lens</div>
             <div class="new_modal__text">wireless brake detection technology, designed to be seen through fog, hail, and poor-visibility weather, whilst maintaining aerodynamics. Sensors work automatically to let others know when you slow down, brake, or down-shift gears.
-                Emergency beacon wile flash in-case of an accident
-                which can be reset manually.</div>
+            Emergency beacon wile flash in-case of an accident
+            which can be reset manually.</div>
+            <div class="l_button active">CONTINUE</div>
+        </div>
+    </div>
+
+    <div class="modal new_modal new_modal-when">
+        <div class="modal__close"></div>
+        <div class="new_modal__wp">
+            <div class="new_modal__title">When Will I get my helmet? helmt my helmet</div>
+            <div class="new_modal__row">
+                <div class="new_modal__block">
+                    <div class="new_modal__white">You are about to place an order for the Alpha Pro UltraModular helmet. A true masterpiece of protective engineering.</div>
+                    <div class="new_modal__grey">The helmet will be ready about 7-9 months from now depending on manufacturing and we hope to ship the first 1500 orders then</div>
+                    <div class="new_modal__red">Early Birds who support us now will benefit from a 15% discount too!</div>
+                </div>
+                <div class="new_modal__center">
+                    <img class="new_modal__arrow" src="img/new_modal__arrow.png" alt="">
+                    <div class="new_modal__txt">OR YOU CAN</div>
+                </div>
+                <div class="new_modal__block">
+                    <div class="new_modal__red">Pay a refundable deposit of £65 to secure one of the first 1500 helmets at the early bird price because prices will increase for all others. The rest you pay when we ship the order.</div>
+                    <div class="new_modal__grey">You will be receiving regular updates whilst we are manufacturing. Prior to shipping pre order customers will have the opportunity to add remove things they like with new additions also at a discounted rate.<br />
+                    Fully refundable deposit in case you change your mind</div>
+                </div>
+            </div>
+            <div class="l_button active">CONTINUE</div>
+            <div class="new_modal__under">Join Us On This Ride!</div>
+        </div>
+    </div>
+
+    <div class="modal new_modal new_modal-size">
+        <div class="modal__close"></div>
+        <div class="new_modal__wp">
+            <div class="new_modal__title">HELMET SIZING AND FITMENT GUIDE</div>
+            <div class="new_modal__gr">Please note a correctly fitted helmet should feel as if an evenly distributed pressure is continuously being exerted 
+            around the head and cheeks. Helmets, like shoes, will break in. For this reason, the helmet should fit as tight as you can 
+            stand to wear it. Once fastened, make sure the helmet cannot be pulled off of the head in any direction.  
+            If the helmet moves significantly, the helmet is too big. First time helmet buyers tend to choose helmets that 
+            are too loose or too big. If the helmet does not fit well, it cannot deliver the level of protection you need.</div>
+            <div class="m_size">
+                <img class="m_size__img" src="img/m_size__img.png" alt="">
+                <div class="m_size__right">	            
+                    <table class="m_size__table">                            
+                        <tr>
+                            <th>ADULT</th>
+                            <th>INCHES</th>
+                            <th>CM</th>
+                        </tr>
+                        <tr>
+                            <td>XS</td>
+                            <td>20.87 - 21.26 in.</td>
+                            <td>53 - 54cm</td>
+                        </tr>
+                        <tr>
+                            <td>S</td>
+                            <td>21.65 - 22.05 in.</td>
+                            <td>55 - 56cm</td>
+                        </tr>
+                        <tr>
+                            <td>M</td>
+                            <td>22.44 - 22.83 in.</td>
+                            <td>57 - 58cm</td>
+                        </tr>
+                        <tr>
+                            <td>L</td>
+                            <td>23.23 - 23.62 in.</td>
+                            <td>59 - 60cm</td>
+                        </tr>
+                        <tr>
+                            <td>XL</td>
+                            <td>24.02 - 24.41 in.</td>
+                            <td>61 - 62cm</td>
+                        </tr>
+                        <tr>
+                            <td>XXL</td>
+                            <td>24.80 - 25.20 in.</td>
+                            <td>63 - 64cm</td>
+                        </tr>
+                        <tr>
+                            <td>XXL</td>
+                            <td>25.60 - 26.00 in.</td>
+                            <td>65 - 66cm</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
             <div class="l_button active">CONTINUE</div>
         </div>
     </div>
@@ -118,7 +195,8 @@
         <div class="o_spoiler">Spoiler</div>
         <div class="o_glasses">Glasses</div>
         <div class="o_blue">Blue</div>
-        <div class="o_lens">Lens</div>
+        <div class="o_when">When</div>
+        <div class="o_size">Size</div>
     </div>
 
 </div>
